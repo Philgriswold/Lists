eae53ac7fb5416d1eeac359b48f4d42eb3ea26f9
Phil Griswold <philgriswold@me.com>
2019-11-15 13:40:48 -0600

    added second exercise

--- Program.cs
+++ Program.cs
@@ -7,34 +7,54 @@ namespace lists
     {
         static void Main(string[] args)
         {
-            List<string> planetList = new List<string>() { "Mercury", "Mars" };
+            // List<string> planetList = new List<string>() { "Mercury", "Mars" };
 
-            planetList.Add("Jupiter");
-            planetList.Add("Saturn");
+            // planetList.Add("Jupiter");
+            // planetList.Add("Saturn");
 
-            //Creates a new list
-            List<string> planetList2 = new List<string>() { "Uranus", "Neptune" };
+            // //Creates a new list
+            // List<string> planetList2 = new List<string>() { "Uranus", "Neptune" };
 
-            //Add the lists together
-            planetList.AddRange(planetList2);
+            // //Add the lists together
+            // planetList.AddRange(planetList2);
 
-            //Inserting new planets
-            planetList.Insert(1, "Venus");
-            planetList.Insert(2, "Earth");
+            // //Inserting new planets
+            // planetList.Insert(1, "Venus");
+            // planetList.Insert(2, "Earth");
 
-            //Adding pluto to the end
-            planetList.Add("Pluto");
-            // planetList.ForEach(planet => Console.WriteLine(planet));
+            // //Adding pluto to the end
+            // planetList.Add("Pluto");
+            // // planetList.ForEach(planet => Console.WriteLine(planet));
 
 
-            // Now that all the planets are in the list, slice the list using GetRange() in 
-            //order to extract the rocky planets into a new list called rockyPlanets. 
-            List<string> rockyPlanets = planetList.GetRange(0, 4);
-            rockyPlanets.ForEach(planets => Console.WriteLine(planets));
+            // // Now that all the planets are in the list, slice the list using GetRange() in 
+            // //order to extract the rocky planets into a new list called rockyPlanets. 
+            // List<string> rockyPlanets = planetList.GetRange(0, 4);
+            // rockyPlanets.ForEach(planets => Console.WriteLine(planets));
 
-            //remove Pluto
-            planetList.Remove("Pluto");
+            // //remove Pluto
+            // planetList.Remove("Pluto");
+
+            Random random = new Random();
+            List<int> numbers = new List<int> {
+            random.Next(6),
+            random.Next(6),
+            random.Next(6),
+            random.Next(6),
+            random.Next(6),
+            random.Next(6),
+            };
+            for (int i = 0; i < numbers.Count; i++)
+                if (numbers.Contains(i))
+                {
+                    Console.WriteLine($"numbers list contains {i}");
+                }
+                else
+                {
+                    Console.WriteLine($"numbers list does not contain {i}");
+                }
         }
-
     }
 }
+
+
